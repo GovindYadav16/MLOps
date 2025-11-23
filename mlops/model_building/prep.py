37bb2283633391e102da13dbb48fd9a3be289eda
Govind Yadav <37355244+GovindYadav16@users.noreply.github.com>
2025-11-23 13:44:42 +0530

    Update prep.py

--- mlops/model_building/prep.py
+++ mlops/model_building/prep.py
@@ -10,7 +10,7 @@ from huggingface_hub import login, HfApi
 
 # Define constants for the dataset and output paths
 api = HfApi(token=os.getenv("HF_TOKEN"))
-DATASET_PATH = "hf://datasets/Bank-Customer-Churn/Bank-Customer-Churn/bank_customer_churn.csv"
+DATASET_PATH = "https://huggingface.co/datasets/yadavgovind/Bank-Customer-Churn/resolve/main/bank_customer_churn.csv"
 bank_dataset = pd.read_csv(DATASET_PATH)
 print("Dataset loaded successfully.")
 
