af5e5684114164973a2c216fb5c4144523b646c1
Govind Yadav <37355244+GovindYadav16@users.noreply.github.com>
2025-11-26 10:25:55 +0530

    Update app.py
    File Name Corrected

--- mlops/deployment/app.py
+++ mlops/deployment/app.py
@@ -4,7 +4,7 @@ from huggingface_hub import hf_hub_download
 import joblib
 
 # Download the model from the Model Hub
-model_path = hf_hub_download(repo_id="yadavgovind/churn-model", filename="best_churn_model_v1.joblib")
+model_path = hf_hub_download(repo_id="yadavgovind/churn-model", filename="churn_prediction_model_v1_0.joblib")
 
 # Load the model
 model = joblib.load(model_path)
